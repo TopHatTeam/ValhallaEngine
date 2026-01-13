5276e01f2b13754e3e55174283e545d6d4d4293b
TophatMusic <SuperMarioAndrew400@gmail.com>
2026-01-13 17:48:01 -0500

    Change window size and update About panel method
    Updated window size to 1920x1080 and modified the About panel to use a new method for displaying application details.

--- ValhallaEngine/main.mm
+++ ValhallaEngine/main.mm
@@ -16,7 +16,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification*)aNotification
 {
-    NSRect WindowContentSize = NSMakeRect(0, 0, 1280, 720);
+    NSRect WindowContentSize = NSMakeRect(0, 0, 1920, 1080);
     
     NSUInteger WindowStyleMask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable | NSWindowStyleMaskMiniaturizable;
     
@@ -26,6 +26,31 @@
     [self.window center];
     [self.window makeKeyAndOrderFront:nil];
     [self.window setDelegate:self];
+    
+    // View layout
+    //{
+    //    ObjectView* ObjViewer = [[ObjectView alloc] initWithFrame:NSMakeRect(0, 0, 200, 720)];
+    //    [self.window.contentView addSubview:ObjViewer];
+    //}
+    
+    NSTabView* EditorTabs = [[NSTabView alloc] initWithFrame:NSMakeRect(0, 0, 1000, 400)];
+    NSTabViewItem* AssetsTab = [[NSTabViewItem alloc] init];
+    [AssetsTab setLabel:@"Assets"];
+    
+    EditorTabs.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+    
+    NSTabViewItem* ClassesTab = [[NSTabViewItem alloc] init];
+    [ClassesTab setLabel:@"Classes"];
+    
+    NSView* FirstTabView = [[NSView alloc] initWithFrame:NSMakeRect(0, 0, 1000, 400)];
+    [FirstTabView setWantsLayer:YES];
+    [FirstTabView.layer setBackgroundColor:[[NSColor grayColor] CGColor]];
+    [AssetsTab setView:FirstTabView];
+    
+    [EditorTabs addTabViewItem:AssetsTab];
+    [EditorTabs addTabViewItem:ClassesTab];
+    
+    [self.window.contentView addSubview:EditorTabs];
     
     [NSApp activateIgnoringOtherApps:YES];
 }
@@ -41,6 +66,18 @@
     return YES;
 }
 
+- (void) showAboutPanel:(id)sender
+{
+    // About information
+    NSDictionary* options =
+    @{
+        @"ApplicationName": @"Valhalla Engine Editor",
+        @"Version": @"1.0",
+        @"Copyright": @"© 2025-2026 Andrew Skatzes"
+    };
+    [NSApp orderFrontStandardAboutPanelWithOptions:options];
+}
+
 @end
 
 int main(int argc, const char * argv[])
@@ -49,6 +86,9 @@ int main(int argc, const char * argv[])
     {
         // Setup code that might create autoreleased objects goes here.
         NSApplication* app = [NSApplication sharedApplication];
+        
+        AppDelegate* appDelegate = [[AppDelegate alloc] init];
+        [app setDelegate:appDelegate];
         
         NSMenu* MainMenu = [[NSMenu alloc] init];
         [NSApp setMainMenu:MainMenu];
@@ -63,16 +103,18 @@ int main(int argc, const char * argv[])
         
         NSString* ApplicationName = [[NSProcessInfo processInfo] processName];
         
-        [ValhallaMenu addItemWithTitle:[@"About " stringByAppendingString:ApplicationName] action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""];
-
+        NSString* AboutTitle = [@"About " stringByAppendingString:ApplicationName];
+        NSMenuItem* AboutItem = [[NSMenuItem alloc] initWithTitle:AboutTitle action:@selector(showAboutPanel:) keyEquivalent:@""];
+        [AboutItem setTarget:appDelegate];
+        
+        [ValhallaMenu addItemWithTitle:[@"About " stringByAppendingString:ApplicationName] action:@selector(showAboutPanel:) keyEquivalent:@""];
+        
         [ValhallaMenu addItem:[NSMenuItem separatorItem]];
         
         [ValhallaMenu addItemWithTitle:[@"Quit " stringByAppendingString:ApplicationName]action:@selector(terminate:) keyEquivalent:@"q"];
-        
-        AppDelegate* appDelegate = [[AppDelegate alloc] init];
-        [app setDelegate:appDelegate];
         
         [app run];
     }
     return 0;
 }
+
