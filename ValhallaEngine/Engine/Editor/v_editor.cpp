b5961d7155d431507edc554256b2cd2596d4bfa1
TophatMusic <SuperMarioAndrew400@gmail.com>
2026-01-24 10:04:44 -0500

    Refactor v_editor.cpp to improve structure and organization

--- ValhallaEngine/Engine/Editor/v_editor.cpp
+++ ValhallaEngine/Engine/Editor/v_editor.cpp
@@ -2,44 +2,89 @@
 //
 //  MIT License
 //
-//  v_editor.cpp
+//  v_editor.hpp
 //  Valhalla Engine Editor
 //
 //  Created by Andrew Skatzes on 1/14/26.
 //
 // -------------------------------------------------
 
-// This is extremely incomplete! Also a shit start
+#pragma once
+#include <QtCore/QtCore>
+#include <QtGui/QAction>
+#include <QtWidgets/QtWidgets>
+#include <QtQuick/QQuickWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QApplication>
 
-#include "headers/v_editor.hpp"
 
-using namespace ValhallaEngine::Editor;
-
-void StartEditor()
+namespace ValhallaEngine::Editor
 {
-    
-}
+    void StartEditor();
 
-void DisplaySplashScreen()
-{
-    
-}
+    void DisplaySplashScreen();
 
-VEditor3DViewer::VEditor3DViewer()
-{
-    WindowViewer = new QQuickWindow;
-    WindowViewer->setColor(Qt::black);
+    class VEditor3DViewer
+    {
+    public:
+        
+        VEditor3DViewer();
+        
+        ~VEditor3DViewer();
+    protected:
+        QQuickWindow* WindowViewer;
+    };
 
-#if defined(__APPLE__)
-    WindowViewer->setGraphicsApi(QSGRendererInterface::Metal);
-#else
-    WindowViewer->setGraphicsApi(QSGRendererInterface::Vulkan);
-#endif
-    
-    WindowViewer->setPersistentSceneGraph(true);
-}
+    class VEditorWindow 
+    {
+    public:
+        VEditorWindow(QMainWindow* MainWindow);
+        ~VEditorWindow();
 
-VEditor3DViewer::~VEditor3DViewer()
-{
-    delete WindowViewer;
+        // The ultimate cheat code for C++
+        // This is allows us unlimited access!
+        // This cannot be safe? Right? 
+        friend class VEditor3DViewer;
+    protected:
+        // --- Menu Bar ---
+        QMenuBar* MenuBar;
+
+        // --- Status Bar ---
+        QStatusBar* StatusBar;
+
+        // --- Menus ---
+        QMenu* ValhallaEngineMenu;
+        QMenu* BuildMenu;
+        QMenu* EditMenu;
+        
+        // --- Valhalla Engine Actions ---
+        QAction* About;
+        QAction* Quit;
+        QAction* NewProject;
+        QAction* OpenProject;
+        QAction* SaveProject;
+        QAction* SaveAs;
+        QAction* WindowsBuild;
+        QAction* MacOSBuild;
+        QAction* LinuxBuild;
+        QAction* CompileSource;
+
+        // --- Edit Actions ---
+        QAction* Copy;
+        QAction* Paste;
+        QAction* Delete;
+
+        // --- Widgets --- 
+        QWidget* CentralWidget;
+        QWidget* TabClasses;
+        QWidget* TabAssets;
+
+        // --- Tab Widget ---
+        QTabWidget* TabEditor;
+    };
+
+    // --- Variables ---
 }
