9fab4b41ec349e6ad1ba2029d2a53f60a6391075
TophatMusic <SuperMarioAndrew400@gmail.com>
2026-01-24 15:09:16 -0500

    Add initial Vulkan backend implementation

---
+++ ValhallaEngine/Engine/GraphicBackend/Vulkan/v_vulkanbackend.cpp
@@ -0,0 +1,24 @@
+// -------------------------------------------------
+//
+//  MIT License
+//
+//  v_vulkanbackend.cpp
+//  Valhalla Engine Editor
+//
+//  Created by Andrew Skatzes on 1/22/26.
+//
+// -------------------------------------------------
+
+#include "v_vulkanbackend.hpp"
+
+using namespace ValhallaEngine::Vulkan;
+
+VVulkanPipeline::VVulkanPipeline(const string& VertexFilepath, const string& FragmentFilepath)
+{
+
+}
+
+VVulkanPipeline::~VVulkanPipeline()
+{
+    
+}
