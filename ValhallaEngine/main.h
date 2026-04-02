8bf595a453134f1e3e471e6fb4580a97564d9148
TophatMusic <SuperMarioAndrew400@gmail.com>
2026-04-02 10:15:32 -0400

    Refactor main.h for platform-specific includes
    Updated header file to include platform-specific imports and adjusted comments.

--- ValhallaEngine/main.h
+++ ValhallaEngine/main.h
@@ -3,7 +3,7 @@
 //  MIT License
 //
 //  main.h
-//  Valhalla Engine Editor
+//  Valhalla Engine 
 //
 //  Created by Andrew Skatzes on 12/31/25.
 //
@@ -12,13 +12,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#import <TargetConditionals.h>
+
+#if defined(__APPLE__) 
+	#import <TargetConditionals.h>
 
 #if TARGET_OS_IOS || TARGET_OS_MACCATALYST
-#import <UIKit/UIKit.h>
+	#import <UIKit/UIKit.h>
+
 #elif TARGET_OS_OSX
-#import <Cocoa/Cocoa.h>
-#else
+	#import <Cocoa/Cocoa.h>
+
+#elif defined(_WIN32) || defined(_WIN64)
+	#include <Windows.h>
+
+#elif defined(__linux__) 
+
+#endif
+
 
 #endif
 
